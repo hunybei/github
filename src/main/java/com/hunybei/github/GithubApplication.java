7933af88985e6287cd654ef14e5e9b8b4552fcda
hunybei <hunybei@foxmail.com>
2019-04-02 00:04:29 +0800

    add hello2 method

--- src/main/java/com/hunybei/github/GithubApplication.java
+++ src/main/java/com/hunybei/github/GithubApplication.java
@@ -13,4 +13,7 @@ public class GithubApplication {
     public void hello1(){
         System.out.println("hello1");
     }
+    public void hello2(){
+        System.out.println("hello2");
+    }
 }
